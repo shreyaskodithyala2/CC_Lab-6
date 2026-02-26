147e2f5a747d958b534332c44d44577275256701
shreyaskodithyala2 <shreyaskodithyala2@gmail.com>
2026-02-26 16:34:31 +0530

    Updated Task 5 load balancing changes

--- backend/app.cpp
+++ backend/app.cpp
@@ -43,12 +43,17 @@ int main() {
         if (client_fd < 0) continue;
         
         // Simple HTTP response
-        std::string response = "HTTP/1.1 200 OK\r\n";
-        response += "Content-Type: text/plain\r\n";
-        response += "Connection: close\r\n\r\n";
-        response += "Served by backend: " + std::string(hostname) + "\n";
-        
-        send(client_fd, response.c_str(), response.length(), 0);
+        std::string body = std::string("Served by backend: ") + std::string(hostname) + "\n";
+
+        std::string response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: " + std::to_string(body.size()) + "\r\n"
+            "Connection: close\r\n"
+            "\r\n" +
+            body;
+
+        send(client_fd, response.c_str(), response.size(), 0);
         close(client_fd);
     }
     
